53859dfb56dcccbfc18b573cb4aad2e7e1f84bef
sulthan-ahmed <sulthanahmeduk@gmail.com>
2015-12-22 21:30:49 +0000

    comment for help with the api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// use this https://apigee.com/console/instagram?req=%7B%22resource%22%3A%22get_media_popular%22%2C%22params%22%3A%7B%22query%22%3A%7B%7D%2C%22template%22%3A%7B%7D%2C%22headers%22%3A%7B%7D%2C%22body%22%3A%7B%22attachmentFormat%22%3A%22mime%22%2C%22attachmentContentDisposition%22%3A%22form-data%22%7D%7D%2C%22verb%22%3A%22get%22%7D
+
 window.Instagram = {
     //Store application settings
      config: {access_token: '2318918744.1fb234f.89b63cc3ffbb4c9eac998e394f26b461'},
